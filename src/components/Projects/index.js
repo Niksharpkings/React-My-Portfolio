46883e371ca6bb6dd1d165cc13426d3584c54320
Nik Sharpio <niksharpkings@gmail.com>
2022-08-20 22:19:51 -0700

    final push

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,12 +1,17 @@
-import proj1 from '../../images/flight.jpg'
-import proj2 from '../../images/proj5.jpg'
-import proj3 from '../../images/proj3.jpg'
-import proj4 from '../../images/proj4.jpg';
+import proj1 from '../../images/184286794-dfe5489c-9e3c-4b15-b536-a0066464bc6f.png'
+import proj2 from '../../images/Screenshot 2022-08-20 220822.png'
+import proj3 from '../../images/181427032-eae96456-d6c7-4b8a-9d2b-b105535d5970.png'
+import proj4 from '../../images/180572719-a97145bc-e921-4c2b-a72d-3919614b314e.png';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 export default function Project(){
     return (
     <div className='card-container'>
-        <div className="card" style={{ width: "40%"}} >
+        <div className="card" style={
+          {
+            width: "40%",
+            height: "40%"
+          }
+        } >
             <img src={proj1} className="card-img-top"  alt="..."/>
             <div className="card-body">
               <h5 className="card-title">
@@ -21,7 +26,12 @@ export default function Project(){
               </h1>
             </div>
         </div>
-        <div className="card" style={{ width: "40%" }} >
+        <div className="card" style={
+          {
+            width: "40%",
+            height: "40%"
+          }
+        } >
             <img src={proj4} className="card-img-top"  alt="..."/>
             <div className="card-body">
             <h5 className="card-title">
@@ -36,8 +46,8 @@ export default function Project(){
             </h1>
             </div>
         </div>
-        <div className="card" style={{ width: "40%" }} >
-            <img src={proj2}  className="card-img-top"  alt="..."/>
+        <div className="card" style={{ width: "40%", height: "40%" }} >
+            <img src={proj2} style={{ width: "100%", height: "40%" }} className="card-img-top"  alt="..."/>
             <div className="card-body">
             <h5 className="card-title">
               <a href="https://sleepy-meadow-10807.herokuapp.com/">
@@ -45,17 +55,17 @@ export default function Project(){
               </a>
             </h5>
             <h1 className="card-text">
-              <a href="https://github.com/ArbiNazari/WanderLust.git">
+              <a href="https://github.com/Niksharpkings/wanderlust">
                 <i className="bi bi-github" />
               </a>
             </h1>
             </div>
         </div>
-        <div className="card" style={{ width: "40%" }} >
-            <img src={proj3} style={{ width: "90%" }}  className="card-img-top"  alt="..."/>
+        <div className="card" style={{ width: "40%", height: "40%" }} >
+            <img src={proj3} style={{ width: "100%", height: "40%" }}  className="card-img-top"  alt="..."/>
             <div className="card-body">
             <h5 className="card-title">
-              <a href="https://github.com/jlemuz/weather-report-app.git">
+              <a href="https://niksharpkings.github.io/Weather-Week-App/">
                 Weather Week App
               </a>
             </h5>
