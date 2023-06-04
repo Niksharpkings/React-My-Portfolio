2a398c81cfacdf9aef9c2d39749cc4354fe9095a
Niksharpkings <Niksharpkings@gmail.com>
2023-06-04 05:42:02 -0700

    v2.0.1 06.04.2023 - updated npm packages

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,14 +5,83 @@ export default function About() {
     <img src={coverImage} style={{ width: "20%" }} alt="cover" />
     <div style={{ width: "60%" }}>
     <h2>Hello,</h2>
-      <p>
-        I am a full stack web developer with a passion for creating beautiful
-        and user-friendly web applications. I have a background in computer
-        science and a passion for creating web applications that are intuitive
-        and easy to use. Im currently looking for a position in the web
-        development industry. If you would like to get in touch, please email
-        me at <a href="mailto: niksharpkings@gmail.com">niksharpkings@gmail</a>
-      </p>
+    <p class="about-me-inner">
+    Currently and intentionally build this very website to demonstrate
+    my ability to build a website from scratch and without any 3rd party
+    site building tools, templates, or libraries, frameworks, or any
+    other, excluding bootstrap, jquery, node.js, npm modules, and so on.
+    currently this site is using only the vanilla HTML5 and
+    <abbr title="cascade style sheet">CSS3</abbr>. I plan to intergrate
+    javascript, bootstrap jquery, node.js packages, npm modules, and so
+    on, in the future. <br><br>
+    I will have and will soon post my React.js, Node.js, Express.js, and
+    MongoDB project here soon (in a few months from March 8, 2023 as
+    this is the last time updated this gitpage site) with
+    profiles/repos/projects/applicaitons that does use javascript
+    scripts, Api, SQL, Apollo, mongodb, CMS, redux, react, angular,
+    ruby, c, C#, C++, python, graphql, yml, php, and so on, on to
+    another GitHub page site.
+    <br><br>
+    Currently, I'm a full stack web developer, with new skills in
+    python, java, c#, c++, c, dotnet, .net in development with the pro
+    edition of codeAcademy.com with a passion for learning these new
+    languages with an attempt to understand every little detail and
+    syntax full and completely.
+    <br><br>
+    I was enrolled in the UCLA extension Full Stack Web Development
+    Coding Bootcamp hosted by U2BootcampNetworks and have successfully
+    gradated.
+    <br><br>
+    Currently I have re-enrolled to Santa Monica College Spring 2023 to
+    pursue a degree in Computer Programming Associate Degree and then I
+    will transfer to CSUN to pursue a Bachelor's and Master Degree in
+    Computer Programming.
+    <br><br>
+    I am not looking for a fwork as I'm commiting myself to full time to
+    achive this master degree mission, but will be looking for maybe a
+    part-time or full-time somewhere near brentwood, Los Angeles,
+    California, but later as I get more confortable with my schooling
+    and schedule.
+    <br><br>
+    In the mean time I will be redoing all myassignments and projects as
+    well as new ones as I progress in Codecademy and in my own personal
+    interest and curiosity in mastering these diverse coding languages.
+    <br><br>
+    I am self-motivated, determinant, dedicated, and commited with
+    diligence and composure in working hard with a passion for learning
+    and problem solving my way into completing projects and resolving
+    bugs and issues in my code to improve and develop my personal coding
+    skills.
+    <br><br>
+    I am a fairly quick learner and a team player, but I do enjoy going
+    slow when it comes to coding as it allows me to make less mistakes
+    and understand or find better or innovative solution and techniques.
+    <br><br>
+    Naturally I'm a uniquely creative problem solver and designer and a
+    good sport or team player when it comes down to it.
+    <br><br>
+    I want to further develop my technological skill sets by taking
+    software development courses and learning the entire windows 10
+    development process and azure, aws, heroku, and google cloud
+    services as well as many language types there are at a controlled
+    and gradual pace.
+    <br><br>
+    Ever since I owned a computer since I was 7 years of age, starting
+    with an old windows 98 and ever since then I been interested in
+    computer technology and how it works, but I never really got into
+    programming until very recently as i figured i didn't really know
+    where a good place to start was.
+    <br><br>
+    Spending most of my time behind a computer screen, I decided to stop
+    being the consumer but to become the developer which is inspiring me
+    to narrow down my life career path into web development, software
+    development, network security, and business intelligence.
+    <br><br>
+    Special thanks to UCLAx-U2bootcampnetwork coding bootcamp got me
+    started/ ball rolling n going into the right direction by helping me
+    finally pick and choose a career that I'm willing to work for an
+    have passion for.
+  </p>
     </div>
   </section>
   );
